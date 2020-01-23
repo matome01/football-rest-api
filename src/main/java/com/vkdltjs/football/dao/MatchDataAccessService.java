91da5d764256300e002982bc8e348acd59a6dc3b
1 <matome000001@gmail.com>
2020-01-24 02:34:14 +0900

    teamId blank OK

--- src/main/java/com/vkdltjs/football/dao/MatchDataAccessService.java
+++ src/main/java/com/vkdltjs/football/dao/MatchDataAccessService.java
@@ -12,14 +12,24 @@ public class MatchDataAccessService implements MatchDAO{
 
     @Override
     public List<Match> getMatches(String from, String to, int teamId) {
-        String sql = "SELECT games.id, play_at, teams1.title AS home, teams2.title AS away, score1 AS home_score, score2 AS away_score FROM games JOIN teams teams1 ON games.team1_id = teams1.id JOIN teams teams2 ON games.team2_id = teams2.id WHERE (teams1.id = ? OR teams2.id = ?) AND games.play_at >= ? AND games.play_at <= ?;";
+        String sql;
+        if(teamId == 0) {
+            sql = "SELECT games.id, play_at, teams1.title AS home, teams2.title AS away, score1 AS home_score, score2 AS away_score FROM games JOIN teams teams1 ON games.team1_id = teams1.id JOIN teams teams2 ON games.team2_id = teams2.id WHERE games.play_at >= ? AND games.play_at <= ?;";
+        }else {
+            sql = "SELECT games.id, play_at, teams1.title AS home, teams2.title AS away, score1 AS home_score, score2 AS away_score FROM games JOIN teams teams1 ON games.team1_id = teams1.id JOIN teams teams2 ON games.team2_id = teams2.id WHERE (teams1.id = ? OR teams2.id = ?) AND games.play_at >= ? AND games.play_at <= ?;";
+        }
         List<Match> matches = new ArrayList<>();
-        try (Connection conn = this.connect();
-            PreparedStatement preparedStatement = conn.prepareStatement(sql)){
-            preparedStatement.setInt(1, teamId);
-            preparedStatement.setInt(2, teamId);
-            preparedStatement.setString(3, from);
-            preparedStatement.setString(4, to);
+        try(Connection conn = this.connect();
+            PreparedStatement preparedStatement = conn.prepareStatement(sql)) {
+            if(teamId == 0) {
+                preparedStatement.setString(1, from);
+                preparedStatement.setString(2, to);
+            }else {
+                preparedStatement.setInt(1, teamId);
+                preparedStatement.setInt(2, teamId);
+                preparedStatement.setString(3, from);
+                preparedStatement.setString(4, to);
+            }
             ResultSet resultSet = preparedStatement.executeQuery();
 
             while(resultSet.next()) {
